cb3b2d891d5ee9ceb1a66e0cddd46bfc242e2219
izan302 <izan302.yt@gmail.com>
2024-10-31 13:36:32 +0100

    Initiated world

--- src/FSM/States/GameStates/Play.lua
+++ src/FSM/States/GameStates/Play.lua
@@ -1,6 +1,7 @@
 --Librerias
 local Object = Object or require "lib/object"
 local Vector = Vector or require "lib/vector"
+local Sti = Sti or require "lib/sti"
 
 --Dependencias
 local Actor = Actor or require "src/actor"
@@ -11,12 +12,20 @@ local House = House or require "src/House"
 
 -- Self
 local PlayState = Object:extend()
+-- Var declarations
+local map, world, tx, ty, patrolPoints
 
 function PlayState:enter()
+    tx, ty = 0, 0
+    map = Sti("src/textures/Map_0.lua", {"box2d"})
+    love.physics.setMeter(32)
+    world = love.physics.newWorld(0, 0)
+    map:box2d_init(world)
+
     love.graphics.setColor(1, 1, 1, 1)
     actorList["Rat"] = Rat(w/2, h/2)
 
-    local patrolPoints = {}
+    patrolPoints = {}
     table.insert(patrolPoints, {x = 20, y = 20})
     table.insert(patrolPoints, {x = 204, y = 20})
     table.insert(patrolPoints, {x = 204, y = 239})
@@ -39,12 +48,15 @@ function PlayState:update(dt)
     for _, v in pairs(actorList) do
         v:update(dt)
     end
+    world:update(dt)
+    map:update(dt)
 end
 
 function PlayState:draw()
     for _, v in pairs(actorList) do
         v:draw()
     end
+    map:draw(-tx, -ty)
 end
 
 function PlayState:exit()
