62ea915b029c33b27de34c49be31a4cb778ad719
izan302 <izan302.yt@gmail.com>
2024-10-31 14:12:23 +0100

    Fixed Maps

--- src/textures/Maps/Tilemap.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="Tilemap" tilewidth="32" tileheight="32" tilecount="35" columns="7">
- <image source="Tilemap.png" width="224" height="160"/>
-</tileset>
