897530c7c27445ba259a25a53a574706d36c4d43
izan302 <izan302.yt@gmail.com>
2024-10-31 11:31:49 +0100

    Added patrol behaviour

--- src/FSM/States/GameStates/Play.lua
+++ src/FSM/States/GameStates/Play.lua
@@ -14,10 +14,28 @@ local PlayState = Object:extend()
 
 function PlayState:enter()
     love.graphics.setColor(1, 1, 1, 1)
-    actorList["Rat"] = Rat(w/2, h/2)    
+    actorList["Rat"] = Rat(w/2, h/2)
+
+    local patrolPoints = {}
+    table.insert(patrolPoints, {x = 20, y = 20})
+    table.insert(patrolPoints, {x = 204, y = 20})
+    table.insert(patrolPoints, {x = 204, y = 239})
+    table.insert(patrolPoints, {x = 454, y = 239})
+    table.insert(actorList, MovingGuard(64, patrolPoints))
+
+    self.toolCooldown = 0.5
+    self.toolCooldownTimer = 0.5 
 end
 
 function PlayState:update(dt)
+    -- TOOL for listing Guard points disable on release
+    self.toolCooldownTimer = self.toolCooldownTimer+dt
+    if love.mouse.isDown(1) and self.toolCooldownTimer > self.toolCooldown then
+        local mouseX, mouseY = love.mouse.getPosition()
+        print("X: "..mouseX.." | Y: "..mouseY)
+        self.toolCooldownTimer = 0
+    end
+    -- END of TOOL
     for _, v in pairs(actorList) do
         v:update(dt)
     end
